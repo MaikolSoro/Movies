f95abf865d0417a7ca57a664ee73fea936ad3303
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-05-22 16:54:37 -0600

    show the posters of movies

--- lib/src/models/movie.model.dart
+++ lib/src/models/movie.model.dart
@@ -60,4 +60,12 @@ class Movie {
     overview = json['overview'];
     releaseDate = json['release_date'];
   }
+  getPosterImg() {
+    if (posterPath == null) {
+      return 'https://lh3.googleusercontent.com/proxy/1SFjiGgcwpHbwY_JtWSTHKJckPQsl9a0A8aK1QteBWeX0ccHFxFrfw73A3VZn2FnJfyWWopiAcesyC_3i2vyi87Tss2O4lyFJDA50kbU2nvEJs_yLoOQKBLjPsXwpy97LL4eSvJyBOnASHTTmdgULZ5IfUSHqV26ZqRax10FvkP3YoTvUj0NN6XIYf6aSk0TGOLBFG0Yvo_s7TVPRo6iTXXsredWzIQHYBq-JH0opc5O-iL3ROjAlClgKbNENvncogq1RmMGum-Khp3x1LtQ5la7mIPBysxOfcHSEAywMKxiu7UII5e0tifB_NWgMX9WR85hEPY9h2cH79EMT6Ix38TP4RGx7tk8xqCg3UDsmSvP7xZf1u9fqsO5Kda72SolRcUBKBwPy7PPvbWnx4BpGcRinpNtazQgOVpLFVwvqlv0jpkF7ze37YLu43ou8vlmXOT5B8z34GvkneZbOM7WksaVDt0b7JlRASN6Ci30eJv3bTsxnk94xiIs8LKaGxHOlKoiHdgu_btFwRD8mT3sWStSldflh8dJifhijdzG6lzy2bMDHSWWD_s8c-K1OZTsoORBoc4';
+    } else {
+      return 'https://image.tmdb.org/t/p/w500/$posterPath';
+
+    }
+  }
 }
